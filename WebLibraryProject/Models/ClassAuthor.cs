876752b1617f7b6e50fa43ee45527466a2547701
Mikhail <miha.vanya@gmail.com>
2018-05-29 19:22:21 +0500

    Renamed everything

---
+++ WebLibraryProject/Models/ClassAuthor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Generator;
+
+namespace WebLibraryProject.Models
+{
+    public partial class DbAuthor
+    {
+        public static List<DbAuthor> All
+        {
+            get
+            {
+                using (var db = new LibraryDBContainer())
+                {
+                    return Enumerable.ToList<DbAuthor>(db.DbAuthorSet1).OrderBy(e => e.WriterType).ToList();
+                }
+            }
+        }
+
+        public string DPublications
+        {
+            get
+            {
+                using (var db = new LibraryDBContainer())
+                {
+                    return db.DbAuthorSet1.Find(Id).Publications.Aggregate(string.Empty, (c, d) => c += $"{d.Name}, ");
+                }
+            }
+        }
+        public eWriterType toEnumWT => (eWriterType)WriterType;
+
+        public DbAuthor(string First, string Last, string Patronimic, eWriterType WriterType) : this()
+        {
+            this.First = First;
+            this.Last = Last;
+            this.Patronimic = Patronimic;
+            this.WriterType = WriterType.e();
+        }
+
+        public static DbAuthor FillBlanks() => FillBlanks((Gender)NewValue.Int(2));
+        public static DbAuthor FillBlanks(Gender gender)
+        {
+            var p = NewPerson.FillBlanks(gender);
+            var b = new DbAuthor(p.First, p.Last, p.Patronimic, (eWriterType)NewValue.Int(2));
+            All.Add(b);
+            return b;
+        }
+        
+        public override string ToString() => $"{Last} {First[0]}.{Patronimic[0]}.";
+        public override int GetHashCode() => ToString().GetHashCode();
+        public override bool Equals(object obj)
+        {
+            var o = obj as DbAuthor;
+            using (var db = new LibraryDBContainer())
+            {
+                return db.DbAuthorSet1.Any(d => d.Id == o.Id &&
+                                               d.First == o.First &&
+                                               d.Last == o.Last &&
+                                               d.Patronimic == o.Patronimic &&
+                                               d.WriterType == o.WriterType);
+            }
+        }
+    }
+
+    public partial class DbAuthor
+    {
+        public DbAuthor ToAuthor()
+        {
+            return new DbAuthor(First, Last, Patronimic, (eWriterType)WriterType)
+            {
+                Id = this.Id,
+                Publications = this.Publications,
+            };
+        }
+    }
+}
---
+++ WebLibraryProject/Models/ClassReader.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Generator;
+
+namespace WebLibraryProject.Models
+{
+
+    public partial class DbReader
+    {
+        public static IEnumerable<string> Groups => All.Select(e => e.Group).Distinct();
+        public static List<DbReader> All
+        {
+            get
+            {
+                using (var db = new LibraryDBContainer())
+                {
+                    return Enumerable.ToList<DbReader>(db.DbReaderSet);
+                }
+            }
+        }
+
+        public int DTakenCount
+        {
+            get
+            {
+                using (var db = new LibraryDBContainer())
+                {
+                    return db.DbReaderSet.Find(Id).PhysicalLocation.Count(e => e.IsTaken);
+                }
+            }
+        }
+        public eAccessLevel toEnumAL => (eAccessLevel) AccessLevel;
+
+        public DbReader(string First, string Last, string Patronimic): this()
+        {
+            this.First = First;
+            this.Last = Last;
+            this.Patronimic = Patronimic;
+            this.AccessLevel = AccessLevel;
+        }
+        public DbReader(string First, string Last, string Patronimic, string Group) : this(First, Last, Patronimic)
+        {
+            AccessLevel = eAccessLevel.Student.e();
+            this.Group = Group;
+        }
+
+        private static string[] GroupNames = new[] { "А", "Ю", "Я", "Ж", "Ё", "ПИ" };
+        public static DbReader FillBlanks() => FillBlanks((Gender) NewValue.Int(2));
+        public static DbReader FillBlanks(Gender gender)
+        {
+            var p = NewPerson.FillBlanks(gender);
+            var b = new DbReader(p.First, p.Last, p.Patronimic, $"{GroupNames.Random()}-{NewValue.Int(15,18)}-{NewValue.Int(1,3)}") { AccessLevel = (byte)NewValue.Int(2) };
+            All.Add(b);
+            return b;
+        }
+        
+        public override string ToString() => $"{Last} {First[0]}. {Patronimic[0]}.";
+        public override int GetHashCode() => ToString().GetHashCode();
+        public override bool Equals(object obj)
+        {
+            var o = obj as DbReader;
+            using (var db = new LibraryDBContainer())
+            {
+                return db.DbReaderSet.Any(d => d.Id == o.Id &&
+                                               d.First == o.First &&
+                                               d.Last == o.Last &&
+                                               d.Patronimic == o.Patronimic &&
+                                               d.Group == o.Group);
+            }
+        }
+    }
+
+    public static partial class Ex
+    {
+        public static DbReader[] Add(this DbReader[] array, DbReader item) => array.Append(item).ToArray();
+
+        public static T Random<T>(this IEnumerable<T> o) => o.ElementAt(NewValue.Int(o.Count()));
+    }
+}
